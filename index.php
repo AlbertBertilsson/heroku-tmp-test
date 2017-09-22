c4f888880df6493d6c5081d77f7d18c486430642
Albert Bertilsson <albert.bertilsson@ikea.com>
2017-09-22 21:35:00 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$content = '';
+$cache_file = '/tmp/map.txt';
+if (file_exists($cache_file)) {
+	$mtime = filemtime($cache_file);
+	if ($mtime > time() - 10) {
+		echo "Use cached\n";
+		$content = file_get_contents($cache_file);
+	} else {
+		echo "Cache expired\n";
+	}
+}
+
+if ($content === '') {
+	echo "Create and cache\n";
+	$content = 'banan';
+	file_put_contents($cache_file, $content);
+}
+
+echo "$content\n";
